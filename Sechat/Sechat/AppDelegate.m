c0c4562ed850f73c8c0ded265ae4162f72dedaa9
Moky <albert.moky@gmail.com>
2019-02-24 23:46:08 +0800

    rename SeChat to Sechat

---
+++ Sechat/Sechat/AppDelegate.m
@@ -0,0 +1,83 @@
+//
+//  AppDelegate.m
+//  Sechat
+//
+//  Created by Albert Moky on 2018/12/21.
+//  Copyright © 2018 DIM Group. All rights reserved.
+//
+
+#import "Facebook.h"
+#import "User.h"
+
+#import "Client.h"
+#import "Station.h"
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    // Override point for customization after application launch.
+    
+    [Facebook sharedInstance];
+    
+#if DEBUG && 1
+    {
+        // moky
+        NSString *path = [[NSBundle mainBundle] pathForResource:@"usr-moky" ofType:@"plist"];
+        DIMUser *user = [DIMUser userWithConfigFile:path];
+        [[Client sharedInstance] addUser:user];
+    }
+#endif
+    
+    // GSP station
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"gsp" ofType:@"plist"];
+    Station *server = [Station stationWithConfigFile:path];
+    [server start];
+    
+    return YES;
+}
+
+
+- (void)applicationWillResignActive:(UIApplication *)application {
+    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+    // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+}
+
+
+- (void)applicationDidEnterBackground:(UIApplication *)application {
+    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    
+    Station *server = [Client sharedInstance].currentStation;
+    [server pause];
+}
+
+
+- (void)applicationWillEnterForeground:(UIApplication *)application {
+    // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+    
+    Station *server = [Client sharedInstance].currentStation;
+    [server resume];
+}
+
+
+- (void)applicationDidBecomeActive:(UIApplication *)application {
+    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+}
+
+
+- (void)applicationWillTerminate:(UIApplication *)application {
+    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    
+    Station *server = [Client sharedInstance].currentStation;
+    [server end];
+}
+
+
+@end
---
+++ Sechat/Sechat/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Sechat
+//
+//  Created by Albert Moky on 2018/12/21.
+//  Copyright © 2018 DIM Group. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
