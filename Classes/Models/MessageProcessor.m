5d5b7062792e7d3ca0b18c5f9602fd39a2df61d1
Moky <albert.moky@gmail.com>
2019-09-08 01:31:48 +0800

    fix an error that conversation data not reload

--- Classes/Models/MessageProcessor.m
+++ Classes/Models/MessageProcessor.m
@@ -77,6 +77,9 @@ SingletonImplementations(MessageProcessor, sharedInstance)
 }
 
 - (NSInteger)numberOfConversations {
+    if (!_conversationList) {
+        [self sortConversationList];
+    }
     return [_conversationList count];
 }
 
